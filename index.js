445f5efaccaffd00b18951acf68d10a98b7ef376
Sugar 2017 <politespring@gmail.com>
2018-01-30 20:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function shout(string){
   `hello!`.toUpperCase()
   return string.toUpperCase()
 }
+function whisper(string){
+  `HELLO`.toLowerCase()
+  return string.toLowerCase()
+}
+function logShout(string){
+  `hello`.toUpperCase()
+  console.log('string'.toUpperCase());
+}
